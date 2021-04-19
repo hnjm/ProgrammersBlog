1c3c880dea05f64297c7a14449e6d12a5460f543
Yunus <yunusozdemir468@gmail.com>
2021-04-19 11:05:13 +0300

    Refactored

--- ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -18,10 +18,7 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
         public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-           // optionsBuilder.UseSqlServer(
-           //     @"Server=(localdb)\ProjectsV13;Database=ProgrammersBlog;Trusted_Connection=True;Connect Timeout=30;MultipleActiveResultSets=True;");
-            optionsBuilder.UseSqlServer(@"Server=localhost;Database=ProgrammersBlog;User=sa;Password=Password123@jkl#;");
-
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=Northwind;User=sa;Password=Password123@jkl#;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
